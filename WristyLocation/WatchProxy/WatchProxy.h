1c0d5a2dc2d04e9749e4c535e942684780859986
Pavan Kumar C <pavan.challa23@gmail.com>
2018-06-12 01:07:07 +0530

    Watch kit added

---
+++ WristyLocation/WatchProxy/WatchProxy.h
@@ -0,0 +1,19 @@
+//
+//  WatchProxy.h
+//  WatchProxy
+//
+//  Created by Pavan Kumar C on 12/06/18.
+//  Copyright © 2018 pavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchProxy.
+FOUNDATION_EXPORT double WatchProxyVersionNumber;
+
+//! Project version string for WatchProxy.
+FOUNDATION_EXPORT const unsigned char WatchProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchProxy/PublicHeader.h>
+
+
